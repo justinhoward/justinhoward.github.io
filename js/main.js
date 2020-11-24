b421e50ae61f58ff0607eab204487b1f4470f83f
Justin Howard <jmhoward0@gmail.com>
2020-11-24 00:36:49 -0800

    Allow toggling to light theme if browser prefers dark

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@
 (function() {
   document.addEventListener('DOMContentLoaded', function(){
     var currentTheme = localStorage.getItem('theme');
+    var prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    if (!currentTheme && prefersDarkScheme) {
+      currentTheme = 'dark'
+    }
+
     var button = document.querySelector('.dark-mode');
     if (currentTheme === 'dark') {
       document.body.classList.add('theme-dark');
@@ -10,15 +15,14 @@
 
     button.addEventListener('click', function(ev) {
       if (currentTheme === 'dark') {
-        currentTheme = undefined;
-        localStorage.removeItem('theme');
+        currentTheme = 'light';
         document.body.classList.remove('theme-dark');
       } else {
         currentTheme = 'dark';
-        localStorage.setItem('theme', currentTheme);
         document.body.classList.add('theme-dark');
       }
 
+      localStorage.setItem('theme', currentTheme);
       ev.preventDefault();
     });
   });
